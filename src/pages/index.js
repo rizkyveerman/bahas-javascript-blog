1ea91db84fe50aa0e70f6390b4c9dfd7c72f3d33
kiky dolphin <rizfirsy@yahoo.com>
2023-01-09 15:50:38 +0700

    remove home features

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import styles from "./index.module.css";
 import Button from "../components/widgets/buttons/Button";
@@ -11,18 +10,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div>
-        <div className={styles.world}>
-          <div className={styles.africa}>
-            <div className={styles.africa1}></div>
-            <div className={styles.africa2}></div>
-            <div className={styles.africa3}></div>
-            <div className={styles.africa4}></div>
-            <div className={styles.africa5}></div>
-            <div className={styles.africa6}></div>
-          </div>
-        </div>
-      </div>
+      <div></div>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -40,9 +28,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <main></main>
     </Layout>
   );
 }
