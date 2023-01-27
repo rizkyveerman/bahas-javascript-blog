381d0f14f80e22ebfd4e4d06521b48cbed4909d3
rizky MSI <rizfirsy@gmail.com>
2023-01-28 01:21:58 +0700

    menyelesaikan tutorial object

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,6 +33,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
         },
         blog: {
           showReadingTime: true,
